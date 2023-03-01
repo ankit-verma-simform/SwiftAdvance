597759f95e8c9e2c64982dfb058b70e5d3ee83a8
ankit-verma-simform <ankit.verma@simformsolutions.com>
2023-03-01 18:32:59 +0530

    covered exceptional handling

---
+++ Playground.playground/Contents.swift
@@ -0,0 +1,28 @@
+
+enum CalcError: Error {
+    case divisionByZeroError
+    case invalidOperationError
+}
+
+do {
+    throw CalcError.divisionByZeroError
+}
+catch CalcError.divisionByZeroError {
+    print("You cannot divide by 0!")
+    do {
+        throw CalcError.divisionByZeroError
+    } catch {
+        print(";;;;;;;")
+    }
+}
+catch {
+    print("Random Error!")
+}
+
+func performDivision(num1: Int, num2: Int) throws -> Int {
+    if num2 == 0 {
+        throw CalcError.invalidOperationError
+    }
+    return num1 / num2
+}
+
