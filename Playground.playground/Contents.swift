7969d09ff8066c101d8a36d24fcda5df769dffef
ankit-verma-simform <ankit.verma@simformsolutions.com>
2023-03-07 17:40:17 +0530

    covered protocols extension, delegation. strong-weak keywords

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -111,7 +111,6 @@ class FanSwitch : Togglable {
 
 var fanSwitch = FanSwitch()
 
-
 protocol Printable {
     var print: String { get }
 }
@@ -140,3 +139,199 @@ class CustomClass: ClassOnlyProtocol {
 //struct CustomStructure: ClassOnlyProtocol {
 //    // won't compile - can't conform to class only protocol
 //}
+
+/*
+ Converting given abstract class example to swift code using
+ protocols - extension
+ 
+ //Java Code
+ 
+ abstract class GraphicObject {
+     int x, y;
+     ...
+     void moveTo(int newX, int newY) {
+         ...
+     }
+     abstract void draw();
+     abstract void resize();
+ }
+ 
+ class Circle extends GraphicObject {
+     void draw() {
+         ...
+     }
+     void resize() {
+         ...
+     }
+ }
+ class Rectangle extends GraphicObject {
+     void draw() {
+         ...
+     }
+     void resize() {
+         ...
+     }
+ }
+ */
+
+protocol GraphicObject {
+    var x: Int { get set }
+    var y: Int { get set }
+    func draw()
+    func resize()
+}
+
+extension GraphicObject {
+    mutating func moveTo(newX: Int, newY: Int) {
+        x = newX
+        y = newY
+    }
+}
+
+class Circle : GraphicObject {
+    var x: Int
+    var y: Int
+    var radius: Int
+    
+    init?(x: Int, y: Int, radius: Int) {
+        guard radius > 0 else {
+            return nil
+        }
+        
+        self.x = x
+        self.y = y
+        self.radius = radius
+    }
+    
+    func draw() {
+        print("Drawing circle with center (\(x),\(y)) and radius \(radius)")
+    }
+    
+    func resize() {
+        print("Resizing circle")
+    }
+}
+
+var circle = Circle(x: 0, y: 0, radius: 5)
+circle?.draw()
+circle?.moveTo(newX: 5, newY: 5)
+circle?.draw()
+
+struct Rectangle : GraphicObject {
+    var x: Int
+    var y: Int
+    var height: Int
+    var weight: Int
+    
+    init?(x: Int, y: Int, height: Int, weight: Int) {
+        guard weight > 0, height > 0 else {
+            return nil
+        }
+        
+        self.x = x
+        self.y = y
+        self.height = height
+        self.weight = weight
+    }
+    
+    func draw() {
+        print("Drawing rectangle with center (\(x),\(y)) [\(height)x\(weight)]")
+    }
+    
+    func resize() {
+        print("Resizing rectangle")
+    }
+}
+
+var rect = Rectangle(x: 14, y: 7, height: 6, weight: 8)
+rect?.draw()
+rect?.moveTo(newX: 76, newY: 9)
+rect?.draw()
+
+// init requirement and required keyword
+
+protocol SomeProtocol {
+    init(someInt: Int)
+}
+
+class SomeClass: SomeProtocol {
+    required init(someInt: Int) {}
+}
+
+// delegation
+
+protocol Worker: AnyObject { //rules to be followed by subtype
+    func doWork()
+}
+
+class Boss {
+    weak var delegate: Worker?
+    
+    func giveTaskToEmployee() { //delegate work to other class
+        delegate?.doWork()
+    }
+}
+
+class Employee: Worker {
+    func doWork() { // knows how to do work
+        print("Does work ...")
+    }
+}
+
+var boss = Boss()
+let employee = Employee()
+boss.delegate = employee
+boss.giveTaskToEmployee()
+
+// strong and weak references
+
+//struct Student {
+//    var name: String
+//    var classmate: Student? // won't compile structs can't have stored property which recursively contains it
+//}
+
+// strong reference example
+class Student {
+    var name: String
+    var classmate: Student?
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    deinit {
+        print("deallocating \(self.name) student!")
+    }
+}
+
+var sam: Student? = Student(name: "Sam") //sam++ -> 1
+var max: Student? = Student(name: "Max") //max++ -> 1
+sam?.classmate = max //max++ -> 2
+max?.classmate = sam //sam++ -> 2
+sam = nil //sam-- -> 1
+max = nil //max-- -> 1
+
+// as reference is not 0, memory is not deallocated!
+
+//weak reference example
+class ChessPlayer {
+    var name: String
+    var rating: Double
+    weak var opponent: ChessPlayer?
+    
+    init(name: String, rating: Double) {
+        self.name = name
+        self.rating = rating
+    }
+    
+    deinit {
+        print("deallocating \(self.name) chess player!")
+    }
+}
+
+var ankit: ChessPlayer? = ChessPlayer(name: "Ankit", rating: 1200) //ankit++ -> 1
+var jap: ChessPlayer? = ChessPlayer(name: "Jap", rating: 1700) //jap++ -> 1
+ankit?.opponent = jap //jap -> 1
+jap?.opponent = ankit //ankit -> 1
+ankit = nil //ankit-- -> 0
+jap = nil //jap-- -> 0
